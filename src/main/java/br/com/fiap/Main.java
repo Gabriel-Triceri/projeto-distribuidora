461d9901207ce9f0b9b636023fca784c0213e9af
Benefrancis <benefrancis@gmail.com>
2023-08-11 21:05:06 -0300

    Mapeamos todas as entidades.

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,27 +1,50 @@
 package br.com.fiap;
 
 import br.com.fiap.domain.entity.Deposito;
+import br.com.fiap.domain.entity.Produto;
+import br.com.fiap.domain.entity.ProdutoEstocado;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
 
-        Deposito depositoNaVilaLeopoldinaDoBrasil = new Deposito();
-        depositoNaVilaLeopoldinaDoBrasil.setId( null ).setNome( "Vila Mariana" );
+        Deposito d1 = new Deposito();
+        d1.setId( null ).setNome( "Vila Mariana" );
+
+        Deposito d2 = new Deposito();
+        d2.setId( null ).setNome( "Vila Maria" );
+
+        var prod1 = new Produto();
+        prod1.setNome( "Oculos" ).setDescricao( "De sol" ).setValor( BigDecimal.valueOf( 699.99 ) );
 
         EntityManagerFactory factory = Persistence.createEntityManagerFactory( "maria-db" );
         EntityManager manager = factory.createEntityManager();
 
         manager.getTransaction().begin();
-        manager.persist( depositoNaVilaLeopoldinaDoBrasil );
-        manager.getTransaction().commit();
+        manager.persist( d1 );
+        manager.persist( d2 );
+        manager.persist( prod1 );
 
+
+        System.out.println( d1 );
+        System.out.println( d2 );
+        System.out.println( prod1 );
+
+        int cont = 0;
+        int qtd = 10;
+        while (cont < qtd) {
+            var item = new ProdutoEstocado();
+            item.setDeposito( d1 ).setProduto( prod1 ).setEntrada( LocalDateTime.now() ).setNumeroDeSerie( cont + "NR" + prod1.getId() );
+            manager.persist( item );
+            cont++;
+        }
+        manager.getTransaction().commit();
         manager.close();
         factory.close();
-
-        System.out.println( depositoNaVilaLeopoldinaDoBrasil );
-
     }
 }
--- src/main/java/br/com/fiap/domain/entity/Deposito.java
+++ src/main/java/br/com/fiap/domain/entity/Deposito.java
@@ -4,14 +4,18 @@ package br.com.fiap.domain.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "TB_DEPOSITO")
+@Table(name = "TB_DEPOSITO", uniqueConstraints = {
+        @UniqueConstraint(name = "UK_NM_DEPOSITO", columnNames = {"NM_DEPOSITO"}) // 1
+})
 public class Deposito {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_DEPOSITO")
     @SequenceGenerator(name = "SQ_DEPOSITO", sequenceName = "SQ_DEPOSITO")
+    @Column(name = "ID_DEPOSITO")
     private Long id;
 
+    @Column(name = "NM_DEPOSITO", nullable = false)
     private String nome;
 
 
